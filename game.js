1f2529532d384cd52e51451ce949b644cb682cab
ellielle <40385743+ellielle@users.noreply.github.com>
2020-04-14 04:03:11 -0400

    added scoreboard and reset

--- game.js
+++ game.js
@@ -28,12 +28,6 @@ const gameBoard = (() => {
       square.textContent = _boardState[index];
     });
   };
-  const _blockInput = () => {
-    document.querySelectorAll('.square').forEach(square => {
-
-      // TODO finish removing event listeners
-    });
-  };
   const _changeColors = () => {
     if (_winningRow.length > 0) {
       _winningRow.forEach(square => {
@@ -45,6 +39,24 @@ const gameBoard = (() => {
       });
     }
   };
+  const _resetBoard = () => {
+    document.querySelectorAll('.square').forEach(square => {
+      square.textContent = '';
+      square.style.color = 'black';
+    });
+    Players.resetScoreChange();
+    _winningRow = [];
+    _boardState.forEach((value, index, arr) => {
+      arr[index] = null;
+    });
+    runGame.setGameOver();
+  };
+  const _updateScore = () => {
+    let player1 = document.querySelector('.player1');
+    let player2 = document.querySelector('.player2');
+    player1.textContent = `P1: ${Players.getScore()[0]}`;
+    player2.textContent = `P2: ${Players.getScore()[1]}`;
+  };
   const createBoardEvents = () => {
     for (let i = 1; i <= 9; i++) {
       let square = _createSquares(i);
@@ -52,25 +64,38 @@ const gameBoard = (() => {
         _takeSquare(i - 1);
       });
     }
+    document.querySelector('.btn-reset').addEventListener('click', _resetBoard);
   };
-  const cleanup = () => {
-    _blockInput();
+  const finishGame = () => {
+    let currentPlayer = runGame.currentTurn() === 1 ? Players.player1 : Players.player2;
     _changeColors();
+    Players.changeScore(currentPlayer);
+    _updateScore(runGame.currentTurn());
   };
   const getBoardState = () => _boardState;
   const setWinningRow = row => _winningRow = row;
-  return { getBoardState, createBoardEvents, cleanup, setWinningRow }
+  return { getBoardState, createBoardEvents, finishGame, setWinningRow }
 })();
 
 const Players = (() => {
   let _p1Wins = 0;
   let _p2Wins = 0;
+  let _allowScoreChange = true;
   const player1 = 'X';
   const player2 = 'O';
   const changeScore = player => {
-    player === 'X' ? _p1Wins++ : _p2Wins++;
+    if (_allowScoreChange) {
+      player === 'X' ? _p1Wins++ : _p2Wins++;
+      _allowScoreChange = !_allowScoreChange;
+    }
   };
-  return { player1, player2, changeScore }
+  const getScore = () => [_p1Wins, _p2Wins];
+  const resetScoreChange = () => {
+    if (runGame.getGameOver()) {
+      _allowScoreChange = !_allowScoreChange;
+    }
+  };
+  return { player1, player2, changeScore, resetScoreChange, getScore }
 })();
 
 const runGame = (() => {
@@ -78,10 +103,10 @@ const runGame = (() => {
   let _gameOver = false;
   const _winConditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
   const _checkForGameOver = board => {
-    let current_player = _turn === 1 ? Players.player1 : Players.player2;
+    let currentPlayer = _turn === 1 ? Players.player1 : Players.player2;
     _winConditions.forEach(condition => {
-      if (board[condition[0]] === current_player && board[condition[1]] === current_player
-        && board[condition[2]] === current_player) {
+      if (board[condition[0]] === currentPlayer && board[condition[1]] === currentPlayer
+        && board[condition[2]] === currentPlayer) {
         _gameOver = true;
         gameBoard.setWinningRow(condition);
       } else if (board.every(value => value != null)) {
@@ -90,20 +115,7 @@ const runGame = (() => {
     });
   };
   const _endGame = () => {
-    gameBoard.cleanup();
-    _showReplayButton();
-    _showScore();
-  };
-
-  const _showReplayButton = () => {
-    // TODO create button, show and allow it to restart game
-  };
-  const _showScore = () => {
-    // TODO add a call to Players.changeScore()
-  };
-  const resetGame = () => {
-    // TODO wipe everything and start over, except score
-    // TODO possibly swap starting player
+    gameBoard.finishGame();
   };
   const changeTurn = () => {
     _checkForGameOver(gameBoard.getBoardState());
@@ -114,5 +126,7 @@ const runGame = (() => {
     }
   };
   const currentTurn = () => _turn;
-  return { changeTurn, currentTurn, resetGame }
+  const getGameOver = () => _gameOver;
+  const setGameOver = () => _gameOver = !_gameOver;
+  return { changeTurn, currentTurn, getGameOver, setGameOver }
 })();
